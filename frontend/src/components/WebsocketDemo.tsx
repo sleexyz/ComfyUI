e5ad5ce866c9ab5c6b5eabd9bbe7e6429722bfc8
Sean Lee <freshdried@gmail.com>
2024-03-02 15:55:09 -0800

    Generate images.

--- frontend/src/components/WebsocketDemo.tsx
+++ frontend/src/components/WebsocketDemo.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import { updatePrompt } from '@/server/generate'
 import { useCallback, useEffect, useRef, useState } from 'react'
 import useSWR from 'swr'
 import { useDebounce } from "use-debounce";
@@ -9,6 +8,8 @@ import { Badge } from './ui/badge';
 import { Skeleton } from './ui/skeleton';
 import { cn } from '@/lib/utils';
 import { v4 as uuidv4 } from 'uuid';
+import actual_best_api_preview from "../../../custom_workflows/actual_best/actual_best_api_preview.json";
+import sixteen_frames_copy_last from "../../../custom_workflows/sixteen_frames_copy_last/sixteen_frames_copy_last_api.json";
 
 const CLIENT_ID = uuidv4();
 
@@ -22,14 +23,13 @@ export function WebsocketDemo() {
     const [promptInput, setPromptInput] = useState('A anime cat');
     const [debouncedPrompt] = useDebounce(promptInput, 200);
 
-    const prompt = useSWR('prompt', () => updatePrompt(debouncedPrompt));
-
     const [currentLog, setCurrentLog] = useState<string>();
 
     const [reconnectCounter, setReconnectCounter] = useState(0)
 
     const canvasRef = useRef<HTMLCanvasElement>(null); // Reference to the canvas element
 
+    const lastLatent = useRef<string>("empty_latent_16.latent.png");
     const sendInput = useCallback(() => {
         if (status == "reconnecting" || status == "connecting")
             return
@@ -43,22 +43,12 @@ export function WebsocketDemo() {
         if (status != "ready")
             return
 
-        if (!prompt.data)
-            return
+        const prompt = generatePrompt({ client_id: CLIENT_ID, inputPrompt: debouncedPrompt, lastLatent: lastLatent.current});
 
-        queuePrompt(prompt.data).then((res) => {
+        queuePrompt(prompt).then((res) => {
             console.log("Prompt queued", res)
         });
-
-        // ws?.send(JSON.stringify(
-        //     {
-        //         "event": "input",
-        //         "inputs": {
-        //             "input_text": debouncedPrompt
-        //         }
-        //     }
-        // ))
-    }, [ws, debouncedPrompt, status, prompt.data])
+    }, [ws, status, debouncedPrompt])
 
     const preStatus = useRef(status)
 
@@ -72,10 +62,11 @@ export function WebsocketDemo() {
         sendInput();
     }, [debouncedPrompt])
 
+
     useEffect(() => {
         setStatus("connecting");
         console.log("connecting to", process.env.NEXT_PUBLIC_COMFY_DEPLOYMENT_WS!);
-        const websocket = new WebSocket(`${process.env.NEXT_PUBLIC_COMFY_DEPLOYMENT_WS!}/ws?client_id=${CLIENT_ID}`);
+        const websocket = new WebSocket(`${process.env.NEXT_PUBLIC_COMFY_DEPLOYMENT_WS!}/ws?clientId=${CLIENT_ID}`);
         websocket.binaryType = "arraybuffer";
         websocket.onopen = () => {
             setStatus("connected");
@@ -83,18 +74,29 @@ export function WebsocketDemo() {
         websocket.onmessage = (event) => {
             if (typeof event.data === "string") {
                 const message = JSON.parse(event.data);
+                console.log("Received message:", message);
                 if (message?.type == "status" && message?.data?.sid) {
                     setStatus("ready");
                 }
-                if (message?.type) {
-                    if (message?.type == "executing" && message?.data?.node == null)
-                        setCurrentLog("done")
-                    else if (message?.type == "live_status")
-                        setCurrentLog(`running - ${message.data?.current_node} ${(message.data.progress * 100).toFixed(2)}%`)
-                    else if (message?.type == "elapsed_time")
-                        setCurrentLog(`elapsed time: ${Math.ceil(message.data?.elapsed_time * 100) / 100}s`)
+                if (message?.type == "execution_start") {
+                    setCurrentLog(`generating...`)
                 }
-                console.log("Received message:", message);
+                if (message?.type == "progress") {
+                    const numerator = message.data?.value || 0;
+                    const denominator = message.data?.max || 1;
+                    setCurrentLog(`generating... ${(numerator/denominator * 100).toFixed(0)}%`)
+                }
+                if (message?.type == "latent-send") {
+                    lastLatent.current = message.data?.images[0].filename;
+                    console.log("updated last latent", lastLatent.current)
+                }
+
+                if (message?.type == "executing" && message?.data?.node == null)
+                    setCurrentLog("done")
+                else if (message?.type == "live_status")
+                    setCurrentLog(`running - ${message.data?.current_node} ${(message.data.progress * 100).toFixed(2)}%`)
+                else if (message?.type == "elapsed_time")
+                    setCurrentLog(`elapsed time: ${Math.ceil(message.data?.elapsed_time * 100) / 100}s`)
             }
             if (event.data instanceof ArrayBuffer) {
                 console.log("Received binary message:");
@@ -205,11 +207,67 @@ export function WebsocketDemo() {
 async function queuePrompt(prompt: any): Promise<any> {
     const p = { prompt, client_id: CLIENT_ID };
     const response = await fetch(`${process.env.NEXT_PUBLIC_COMFY_DEPLOYMENT_HTTP!}/prompt`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(p),
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(p),
     });
     return response.json();
-  }
+}
+
+function clone(prompt: any) {
+    return JSON.parse(JSON.stringify(prompt));
+}
+
+type Prompt = Record<string, PromptNode>;
+interface PromptNode {
+    class_type: string;
+    inputs: Record<string, any>;
+    _meta?: Record<string, any>;
+}
+
+function getNodeWithClassType(prompt: Prompt, classType: string): string | undefined {
+    for (const [key, value] of Object.entries(prompt)) {
+        if (value.class_type === classType) {
+            return key;
+        }
+    }
+    return undefined;
+}
+
+function getNodeWithTitle(prompt: Prompt, title: string): string {
+    for (const [key, value] of Object.entries(prompt)) {
+        if (value._meta?.title === title) {
+            return key;
+        }
+    }
+    throw new Error(`No node with title ${title} found in the prompt`);
+}
+
+// export async function updatePrompt(firstBatch: boolean, inputPrompt: string) {
+export function generatePrompt({ client_id, inputPrompt, lastLatent }: { client_id: string, inputPrompt: string, lastLatent: string}) {
+    const prompt = clone(sixteen_frames_copy_last);
+
+    // Set the text prompt for our positive CLIPTextEncode
+    const clipTextEncodeNodeId = getNodeWithTitle(prompt, "CLIP Text Encode (Prompt)");
+    if (!clipTextEncodeNodeId) {
+        throw new Error("No CLIP Text Encode (Prompt) node found in the prompt");
+    }
+    prompt[clipTextEncodeNodeId]["inputs"]["text"] = inputPrompt;
+
+    // if (firstBatch) {
+    const latentRecieverNodeId = getNodeWithClassType(prompt, "LatentReceiver");
+    if (!latentRecieverNodeId) {
+        throw new Error("No LatentReceiver node found in the prompt");
+    }
+    prompt[latentRecieverNodeId]["inputs"]["latent"] = `latents/${lastLatent} [temp]`;
+
+    const wsNodeId = getNodeWithClassType(prompt, "ComfyDeployWebscoketImageOutput");
+    if (!wsNodeId) {
+        throw new Error("No ComfyDeployWebscoketImageOutput node found in the prompt");
+    }
+    prompt[wsNodeId]["inputs"]["client_id"] = client_id;
+
+    return prompt;
+}
